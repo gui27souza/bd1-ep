40ba883947e2a630a5c6f02e68ce7bfe45027ba1
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-09-27 19:48:28 -0300

    adiciona env no script de execução

--- run_ep.sh
+++ run_ep.sh
@@ -1,3 +1,8 @@
+export EP_DB="ip e porta"
+export EP_NAME_DB="nome BD"
+export EP_DB_USER="usuario BD (default: postgres)"
+export EP_DB_PASS="Senha do user"
+
 # Cria o bin/ se necessário
 mkdir -p bin
 
