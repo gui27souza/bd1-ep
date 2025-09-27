447b9e384a9d05538cebf7e56d373f2e070bb904
Kennedy Menezes <menezes_kennedy@hotmail.com>
2025-09-27 10:31:53 -0300

    adicionando ddl

---
+++ ep/DDL.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE planejei;
+
+CREATE TABLE plano (
+    id INT NOT NULL PRIMARY KEY,
+    nome VARCHAR(50),
+    valor DECIMAL(10,2),
+	qtd_convites INTEGER
+);
+
+CREATE TABLE cliente(
+	id INTEGER NOT NULL PRIMARY KEY,
+	nome VARCHAR(50),
+	cpf VARCHAR(11) UNIQUE,
+	data_nasc DATE,
+	id_plano INTEGER,
+	FOREIGN KEY (id_plano) REFERENCES plano(id)
+);
