2a775394b13305eced08a5699d0f27de2fce1105
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-11-06 19:23:41 -0300

    feat: construtor conector SQL

--- src/main/java/DBConnector.java
+++ src/main/java/DBConnector.java
@@ -5,4 +5,43 @@ import java.sql.*;
 
 public class DBConnector {
 
+	Connection conn = null;
+
+	public DBConnector() {
+		this.conn = createConnection();
+	}
+
+	private Connection createConnection() {
+
+		String host;
+		String dbName;
+		String user;
+		String password;
+		String url;
+
+		try {
+			host = System.getenv("EP_DB");
+			dbName = System.getenv("EP_NAME_DB");
+			user = System.getenv("EP_DB_USER");
+			password = System.getenv("EP_DB_PASS");
+			url = String.format("jdbc:postgresql://%s/%s", host, dbName);
+		} catch (Exception e) {
+			System.out.println("Erro ao buscar variáveis de ambiente para conexão com o banco de dados:\n"+e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
+
+		Connection conn;
+
+		try{
+			conn = DriverManager.getConnection(url, user, password);
+			System.out.println("\nConexão estabelecida com sucesso!\n");
+		} catch (SQLException e) {
+			System.err.println("Erro ao tentar conectar ao banco de dados:\n"+e.getMessage());
+			e.printStackTrace();
+			return null;
+		}
+
+		return conn;
+	}
 }
