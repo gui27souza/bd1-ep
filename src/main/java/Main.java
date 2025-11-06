b73926e3f5f2925b7c9e2aadcebe9667bd1b3b5b
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-11-06 20:41:18 -0300

    feat: exemplo de Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,15 @@
 package main.java;
 
-public class Main {
-	public static void main(String[] args) {
+import java.sql.SQLException;
 
+public class Main {
+	public static void main(String[] args) throws SQLException {
+
+		DBConnector db = new DBConnector();
+		System.out.println("Tabelas disponíveis no BD:\n"+db.availableTables+"\n");
+
+		for (String tableName: db.availableTables) {
+			Menu.printTabela(tableName, db);
+		}
+	}
 }
