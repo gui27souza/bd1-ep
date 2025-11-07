c8fc198150cec4c8bca956e2d8ab04264155844b
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-11-07 20:20:14 -0300

    feat: método para inserir um novo cliente no BD

--- src/main/java/service/ClienteService.java
+++ src/main/java/service/ClienteService.java
@@ -17,9 +17,53 @@ public class ClienteService {
 
 	DBConnector dbConnector = null;
 
+	int idPlanoBasico = 1;
 
 	public ClienteService(DBConnector dbConnector) {
 		this.dbConnector = dbConnector;
 	}
 
+	public Cliente createCliente(String nome, long cpf, Date dataNascimento) throws SQLException, DomainException {
+
+		if (nome == null || nome.trim().isEmpty()) {
+			throw new DomainException("O nome do cliente não pode ser vazio.");
+		}
+		if (cpf <= 0) {
+			throw new DomainException("CPF inválido.");
+		}
+		
+		Cliente novoCliente = new Cliente(nome, cpf, dataNascimento, this.idPlanoBasico);
+
+		String query = "INSERT INTO cliente (nome, cpf, data_nasc, id_plano) VALUES (?, ?, ?, ?) RETURNING id";
+
+		try (PreparedStatement pstmt = this.dbConnector.getConnection().prepareStatement(query)) {
+
+			pstmt.setString(1, novoCliente.getNome());
+			pstmt.setLong(2, novoCliente.getCpf());
+			pstmt.setDate(3, novoCliente.getDataNascimento());
+			pstmt.setInt(4, novoCliente.getIdPlano());
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+
+				if (rs.next()) {
+
+					int novoId = rs.getInt("id");
+
+					novoCliente.setId(novoId);
+
+					return novoCliente;
+				} else {
+					throw new SQLException("Falha ao obter o ID do cliente após a inserção.");
+				}
+			}
+
+		} catch (SQLException e) {
+
+			if (e.getSQLState().equals("23505")) {
+				throw new DomainException("Já existe um cliente cadastrado com este CPF.");
+			}
+
+			throw e;
+		}
+	}
 }
