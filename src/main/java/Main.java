0272bbc73b2eae7ba9cbb5aab86f351a80522fc5
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-11-07 10:53:41 -0300

    feat: menu interativo no Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,47 @@ public class Main {
 	public static void main(String[] args) throws SQLException {
 
 		DBConnector db = new DBConnector();
-		System.out.println("Tabelas disponíveis no BD:\n"+db.availableTables+"\n");
 
-		for (String tableName: db.availableTables) {
-			Menu.printTabela(tableName, db);
+		String[] primeirasOpcoes = {
+			"Ver todas as tabelas",
+			"Buscar tabela específica",
+			"Listar o nome das tabelas",
+		};
+
+		while (true) {
+
+			int opt = Menu.printOptions(primeirasOpcoes);
+
+			switch (opt) {
+
+				case -1:
+					System.out.println("\nEncerrando programa...");
+					System.exit(0);
+				break;
+
+				case 0:
+					for (String tableName : db.availableTables) {
+						Menu.printTabela(tableName, db);
+						System.out.println();
+					}
+				break;
+
+				case 1:
+					System.out.println();
+					String tableName = Menu.readStringInput("Digite o nome da tabela: ");
+					Menu.printTabela(tableName, db);
+//					System.out.println("\nOperação ainda não implementada!");
+				break;
+
+				case 2:
+					System.out.println("\nTabelas disponíveis no BD:\n" + db.availableTables + "\n");
+				break;
+
+			}
+
+			System.out.println();
 		}
+
+
 	}
 }
