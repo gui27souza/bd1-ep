6658be74cf70e9ea1ddd1971206db6f3e42ddd08
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-11-08 17:07:32 -0300

    feat: print de cliente e lista cliente

--- src/main/java/util/menu/MenuUtilCliente.java
+++ src/main/java/util/menu/MenuUtilCliente.java
@@ -2,6 +2,20 @@ package main.java.util.menu;
 
 import main.java.model.cliente.Cliente;
 
+import java.util.ArrayList;
 
 public class MenuUtilCliente {
+
+	public static void printCliente(Cliente cliente) {
+		System.out.println("\n====================");
+		cliente.repr();
+		System.out.println("====================");
+	}
+
+	public static void printListaCliente(ArrayList<Cliente> listaClientes) {
+		for (Cliente cliente: listaClientes) {
+			printCliente(cliente);
+		}
+	}
+
 }
