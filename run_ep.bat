400132805ef96aea874cd7cb5274fe1b89737e37
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-11-08 17:17:26 -0300

    dev: ajuste nos scripts de execução

--- run_ep.bat
+++ run_ep.bat
@@ -5,13 +5,13 @@ REM Compila, Executa e Limpa.
 REM =======================================================
 
 REM 1. Compila
-javac -encoding UTF-8 -d . -cp lib\postgresql-42.7.8.jar src\main\java\*.java
+javac -encoding UTF-8 -d . -cp lib\postgresql-42.7.8.jar src\main\java\**\*.java
 
 REM Verifica se a compilação falhou (código de erro > 0)
 if errorlevel 1 goto :EOF
 
 REM 2. Executa
-java -cp .:lib/postgresql-42.7.8.jar main.java.Main
+java -cp .:lib/postgresql-42.7.8.jar main.java.managerapp.Main
 
 REM 3. limpa
 rmdir /s /q main
