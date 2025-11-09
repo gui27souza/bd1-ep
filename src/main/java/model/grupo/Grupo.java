b9a6cc45c1295a71f0c69f0d5840b05d952f8815
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-11-09 14:39:53 -0300

    feat: getters/setters para Grupo

--- src/main/java/model/grupo/Grupo.java
+++ src/main/java/model/grupo/Grupo.java
@@ -1,15 +1,16 @@
 package main.java.model.grupo;
 
+import main.java.exceptions.DomainException;
 import main.java.model.cliente.Cliente;
 
 import java.util.ArrayList;
 
 public class Grupo {
 
-	int id;
+	final int id;
 	String nome;
 	GrupoStatus status;
-	String dataCriacao;
+	final String dataCriacao;
 	String descricao;
 	ArrayList<Cliente> clientes;
 
@@ -22,4 +23,38 @@ public class Grupo {
 		this.clientes = new ArrayList<>();
 	}
 
+	public int getId() { return id;	}
+	public String getNome() { return nome; }
+	public GrupoStatus getStatus() { return status;	}
+	public String getDataCriacao() { return dataCriacao; }
+	public String getDescricao() { return descricao; }
+	public ArrayList<Cliente> getClientes() { return new ArrayList<>(this.clientes); }
+
+	public void setNome(String nome) { this.nome = nome; }
+	public void setStatus(GrupoStatus status) { this.status = status; }
+	public void setDescricao(String descricao) { this.descricao = descricao; }
+
+	public void addCliente(Cliente cliente) throws DomainException {
+
+		if (this.clientes.contains(cliente)) {
+			throw new DomainException(
+				"Cliente " + cliente.getNome() + "(id: " + cliente.getId() + ")" +
+				" já está no grupo " + this.nome + "(id: " + this.id + ")"
+			);
+		}
+
+		this.clientes.add(cliente);
+	}
+
+	public void removeCliente(Cliente cliente) throws DomainException {
+
+		if (!this.clientes.contains(cliente)) {
+			throw new DomainException(
+				"Cliente " + cliente.getNome() + "(id: " + cliente.getId() + ")" +
+				" não está no grupo " + this.nome + "(id: " + this.id + ")"
+			);
+		}
+
+		this.clientes.remove(cliente);
+	}
 }
