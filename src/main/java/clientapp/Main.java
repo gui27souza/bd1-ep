9b702d31c4c1345ff73e1cffcb4dd802c29ad22d
Guilherme27Souza <guilhermeoliveiradesouza@gmail.com>
2025-11-09 14:43:08 -0300

    feat: inicialização dos serviços em clientapp

--- src/main/java/clientapp/Main.java
+++ src/main/java/clientapp/Main.java
@@ -1,11 +1,16 @@
 package main.java.clientapp;
 
+import main.java.db.DBConnector;
 import main.java.exceptions.DomainException;
+import main.java.model.acesso.Acesso;
+import main.java.service.CadastroService;
+import main.java.service.ClienteService;
 import main.java.util.menu.MenuUtil;
 
 import java.sql.SQLException;
 
 public class Main {
+
 	public static void main(String[] args) throws DomainException, SQLException {
 
 		String[] menuOptions = {
@@ -13,8 +18,13 @@ public class Main {
 			"Fazer cadastro"
 		};
 
+		DBConnector dbConnector = new DBConnector();
+		ClienteService clienteService = new ClienteService(dbConnector);
+		CadastroService cadastroService = new CadastroService(dbConnector);
 
-		while (acessoAtual != null) {
+		Acesso acessoAtual = null;
+
+		while (acessoAtual == null) {
 
 			int opt = MenuUtil.printOptions(menuOptions);
 
